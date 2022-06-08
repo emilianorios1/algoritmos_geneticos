660397967ae931cd83434668985e73583a6456ff
emilianorios1 <emilianorios99@gmail.com>
2022-06-07 22:56:51 -0300

    cambios funcion combinations

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 volumenes = [150, 325, 600, 805, 430, 1200, 770, 60, 930, 353]
+
 valores = [20, 40, 50, 36, 25, 64, 54, 18, 46, 28]
+
 volumen_mochila = 4200
 
 #division = []
@@ -19,29 +21,11 @@ volumen_mochila = 4200
 # c= [[], [0], [1],[0, 1], [2], [0,2]] -> 
 # c= [[], [0], [1],[0, 1], [2], [0,2], [1,2]]  -> 
 # c= [[], [0], [1],[0, 1], [2], [0,2], [1,2], [0,1,2]]  -> 
-#                                            //^ acá se suma [0,1] + [2]//                                                                            ^ acá se suma [] + [2]
-
-def combinations(l):
+#                                            //^ acá se suma [0,1] + [2]//
+def combinations():
   c = [[]]
-  for x in l:
+  for x in range(len(valores)):
       for r in c:
         c= c + [r+[x]]
   return c
-
-combinaciones_valores = combinations(valores)
-combinaciones_volumenes = combinations(volumenes)
-
-
-max_valor = 0
-n_mejor = 0
-
-for i in range(len(combinaciones_valores)):
-  
-  if(sum(combinaciones_valores[i]) > max_valor and sum(combinaciones_volumenes[i]) < volumen_mochila):
-    n_mejor = i
-    max_valor = sum(combinaciones_valores[i])
-
-print("La mejor combinación es ", n_mejor)
-
-print("valor total    ", sum(combinaciones_valores[n_mejor]))
-print("volumen total  ", sum(combinaciones_volumenes[n_mejor]))
+combinaciones = combinations()
