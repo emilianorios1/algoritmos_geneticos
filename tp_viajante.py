4eeeb832e201453495e86434d50bddf4979484a8
emiliano <erios@comparaonline.com>
2022-09-26 01:26:13 -0300

    creamos el grafico en un mapa del recorrido del viajante

--- tp_viajante.py
+++ tp_viajante.py
@@ -1,37 +1,50 @@
+import os
 
 import numpy as np
 import pandas as pd
+import folium
+from folium.plugins import Search
 
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-from cartopy.io.shapereader import Reader as sf
-from cartopy.feature import ShapelyFeature as cfeature
-
-
-#El archivo shp lo pueden encontrar en la página del Instituto Geográfico Nacional (IGN)
-fname = 'provincia.shp'
-
-fig, ax = plt.subplots( 1, 1, subplot_kw=dict(projection=ccrs.PlateCarree()), figsize=[12,14]  )
-shape_feature = cfeature(sf(fname).geometries(), ccrs.PlateCarree(), edgecolor='black')
-ax.add_feature(shape_feature, facecolor= 'none')
-
-#Defino los ejes (lon min y max, lat min y max)
-ax.set_extent([-76, -53, -57, -20])
-
-data = pd.read_excel('TablaCiudades.xlsx', header=0)
+data = pd.read_excel('TablaCapitales.xlsx', header=0)
 ciudades = list(data)
 distancias = data.to_numpy()
+lat_lon = [[-34.6212767, -58.4145884],
+           [-31.3994342, -64.2643837],
+           [-27.4862473, -58.8274061],
+           [-26.1721517, -58.2299893],
+           [-34.9205233, -57.9881897],
+           [-29.4142924, -66.8907965],
+           [-32.883334, -68.8760287],
+           [-38.9412294, -68.1504201],
+           [-31.7472984, -60.5867216],
+           [-27.396305, -55.9595352],
+           [-43.2988158, -65.1206438],
+           [-27.4606431, -59.0671039],
+           [-51.6264434, -69.3067685],
+           [-28.4645296, -65.8101793],
+           [-26.8328546, -65.2576131],
+           [-24.2053356, -65.3405747],
+           [-24.7960685, -65.5006685],
+           [-31.5317743, -68.5501862],
+           [-33.297652, -66.3446851],
+           [-31.6181226, -60.7780156],
+           [-36.6193822, -64.336255],
+           [-27.8017104, -64.3020677],
+           [-54.8068581, -68.3378226],
+           [-40.8250227, -63.0176492]
+           ]
 
 
-def combinations(lista): # Usado para armar una búsqueda exhaustiva.
+def combinations(lista):  # Usado para armar una búsqueda exhaustiva.
     comb = [[]]
     cont = 0
     for x in lista:
         for r in comb:
-            comb = comb + [r+[x]]
-            cont = cont+1
+            comb = comb + [r + [x]]
+            cont = cont + 1
             print(cont)
     return comb
+
 
 # combinations(ciudades)
 
@@ -40,7 +53,7 @@ def calcular_distancia_total():
     acum = 0
     for i in range(len(ciudades_visitadas) - 1):
         ciudad_origen = ciudades_visitadas[i]
-        ciudad_final = ciudades_visitadas[i+1]
+        ciudad_final = ciudades_visitadas[i + 1]
         acum += distancias[ciudad_origen][ciudad_final]
     return acum
 
@@ -55,15 +68,15 @@ def visitar_ciudad_mas_cerana(partida):
 
 # MENU
 
-for i in range(len(ciudades)):
-    print(i, " ", ciudades[i])
-partida = int(input("Ingrese número de ciudad inicial: "))
-print(ciudades[partida])
+# for i in range(len(ciudades)):
+#    print(i, " ", ciudades[i])
+# partida = int(input("Ingrese número de ciudad inicial: "))
+# print(ciudades[partida])
 
 ##
 
 
-#partida = 4
+partida = 0
 
 ciudades_visitadas = [partida]  # Creamos el arreglo ciudades_visitadas con partida como único elemento.
 
@@ -76,5 +89,17 @@ print("Recorrido: ")
 for i in ciudades_visitadas:
     print(ciudades[i])
 
-print("Distancia total: ", calcular_distancia_total())
+print("Distancia total recorrida: ", calcular_distancia_total())
 
+
+m = folium.Map(location=[-34.603722, -58.381592], zoom_start=4)  # Inicializa el mapa en Argentina.
+
+for i in range(len(ciudades)):  # Hace marcadores en todas las capitales.
+    folium.Marker(location=lat_lon[i], popup=ciudades[i]).add_to(m)
+
+lat_lon_visitados = []
+for i in ciudades_visitadas:  # Genera una lista con las latitudes y longitudes en el orden que se visitaron.
+    lat_lon_visitados.append(lat_lon[i])
+folium.PolyLine(locations=lat_lon_visitados, color='red').add_to(m)  # Genera las lineas del recorrido
+
+m.save("index.html")
